eda2989d3969871fb040f9770cc536056ace8721
Elina Lua Ming <ardentlycurious101@gmail.com>
2023-05-19 23:03:58 -0700

    Create DirectGeocodingInteractor.swift

---
+++ Weather/Interactor/DirectGeocodingInteractor.swift
@@ -0,0 +1,29 @@
+//
+//  DirectGeocodingInteractor.swift
+//  Weather
+//
+//  Created by Elina Lua Ming on 5/19/23.
+//
+
+import Foundation
+
+class DirectGeocodingInteractor {
+    
+    let networking = DirectGeocodingNetworking()
+    let debouncer = Debouncer(debounceTime: 0.3) // 300 ms debounce
+
+    func perform(for input: String, completion: @escaping (_ result: [SearchResultElement]?) -> Void) {
+        // Debounce input by 300 ms, so that we're not refetching with every new character typed to avoid making too many unnecessary API calls
+        debouncer.call {
+            self.networking.fetchResult(for: input) { result, error in
+                guard error == nil else {
+                    print(error!)
+                    return
+                }
+                
+                completion(result ?? nil)
+            }
+        }
+    }
+    
+}
