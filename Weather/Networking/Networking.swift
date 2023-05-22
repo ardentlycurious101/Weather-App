845ff6b70ba380df6b31a44211459b560af4dd96
Elina Lua Ming <ardentlycurious101@gmail.com>
2023-05-21 21:03:49 -0700

    Refactor the networking files into a single networking file `Networking`

--- Weather/Networking/Networking.swift
+++ Weather/Networking/Networking.swift
@@ -1,26 +1,26 @@
 //
-//  WeatherNetworking.swift
+//  Networking.swift
 //  Weather
 //
-//  Created by Elina Lua Ming on 5/19/23.
+//  Created by Elina Lua Ming on 5/21/23.
 //
 
 import Foundation
 import Moya
 
-class WeatherNetworking {
-    let provider = MoyaProvider<WeatherAPI>()
+class Networking<T: TargetType> {
+    let provider = MoyaProvider<T>()
     
-    func fetchWeather(for location: (lat: String, lon: String), completion: @escaping (_ cityWeather: CityWeather?, _ error: Error?) -> Void) {
+    func fetch<R: Decodable>(api: T, completion: @escaping (_ results: R?, _ error: Error?) -> Void) {
         do {
             // Retryable up to 5 times if request fails with 3s retry interval.
             let _ = try Retry.perform(maximumTries: 5, retryInterval: 3.0) {
-                self.provider.request(.fetchWeather(location: location)) { result in
+                self.provider.request(api) { result in
                     switch result {
                     case .success(let response):
                         do {
-                            let cityWeather = try response.filterSuccessfulStatusCodes().map(CityWeather.self)
-                            completion(cityWeather, nil)
+                            let decodedResult = try response.filterSuccessfulStatusCodes().map(R.self)
+                            completion(decodedResult, nil)
                         } catch let error {
                             print(error.localizedDescription)
                             completion(nil, error)
