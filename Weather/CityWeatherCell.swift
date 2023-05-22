29cbcda51c4f8c1ceab9cf559158b2c3f435d73b
Elina Lua Ming <ardentlycurious101@gmail.com>
2023-05-21 19:59:46 -0700

    Update CityWeatherCell - description strings, add container for inset

--- Weather/CityWeatherCell.swift
+++ Weather/CityWeatherCell.swift
@@ -10,6 +10,12 @@ import UIKit
 class CityWeatherCell: UICollectionViewCell {
     static let identifier = "CityWeatherCell"
     
+    lazy var containerView: UIView = {
+        let view = UIView(frame: .zero)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     lazy var cityNameView: UILabel = {
         let view = UILabel(frame: .zero)
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -66,32 +72,38 @@ class CityWeatherCell: UICollectionViewCell {
         self.contentView.layer.cornerCurve = .continuous
         self.contentView.clipsToBounds = true
         
-        self.contentView.addSubview(cityNameView)
-        self.contentView.addSubview(descriptionView)
-        self.contentView.addSubview(mainTempView)
-        self.contentView.addSubview(maxTempView)
-        self.contentView.addSubview(minTempView)
+        self.contentView.addSubview(containerView)
+        self.containerView.addSubview(cityNameView)
+        self.containerView.addSubview(descriptionView)
+        self.containerView.addSubview(mainTempView)
+        self.containerView.addSubview(maxTempView)
+        self.containerView.addSubview(minTempView)
         
         NSLayoutConstraint.activate([
-            cityNameView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor),
-            cityNameView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor),
-            cityNameView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor),
+            containerView.topAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.topAnchor, constant: 20),
+            containerView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            containerView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            containerView.bottomAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+
+            cityNameView.topAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.topAnchor),
+            cityNameView.leadingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.leadingAnchor),
+            cityNameView.trailingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.trailingAnchor),
             
-            descriptionView.topAnchor.constraint(equalTo: cityNameView.bottomAnchor),
-            descriptionView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor),
-            descriptionView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor),
+            descriptionView.topAnchor.constraint(equalTo: cityNameView.bottomAnchor, constant: 10),
+            descriptionView.leadingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.leadingAnchor),
+            descriptionView.trailingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.trailingAnchor),
             
             mainTempView.topAnchor.constraint(equalTo: descriptionView.bottomAnchor),
-            mainTempView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor),
-            mainTempView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor),
+            mainTempView.leadingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.leadingAnchor),
+            mainTempView.trailingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.trailingAnchor),
             
             maxTempView.topAnchor.constraint(equalTo: mainTempView.bottomAnchor),
-            maxTempView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor),
-            maxTempView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor),
+            maxTempView.leadingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.leadingAnchor),
+            maxTempView.trailingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.trailingAnchor),
             
             minTempView.topAnchor.constraint(equalTo: maxTempView.bottomAnchor),
-            minTempView.leadingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.leadingAnchor),
-            minTempView.trailingAnchor.constraint(equalTo: contentView.safeAreaLayoutGuide.trailingAnchor),
+            minTempView.leadingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.leadingAnchor),
+            minTempView.trailingAnchor.constraint(equalTo: containerView.safeAreaLayoutGuide.trailingAnchor),
         ])
     }
     
@@ -99,22 +111,27 @@ class CityWeatherCell: UICollectionViewCell {
         self.cityNameView.text = cityName
     }
     
-    func setWeatherData(with cityWeather: CityWeather) {
+    func setWeatherData(with cityWeather: CityWeather?) {
+        
+        guard let cityWeather = cityWeather else {
+            descriptionView.text = "Loading ..."
+            return
+        }
         
         if let weatherDescription = cityWeather.weather?.first?.description {
             descriptionView.text = weatherDescription.capitalized
         }
         
         if let mainTemp = cityWeather.main?.temp {
-            mainTempView.text = String(mainTemp) + "°"
+            mainTempView.text = "Temperature: " + String(mainTemp) + "°F"
         }
         
         if let maxTemp = cityWeather.main?.tempMax {
-            maxTempView.text = String(maxTemp) + "°"
+            maxTempView.text = "Max Temperature: " + String(maxTemp) + "°F"
         }
         
         if let minTemp = cityWeather.main?.tempMin {
-            minTempView.text = String(minTemp) + "°"
+            minTempView.text = "Min Temperature: " + String(minTemp) + "°F"
         }
     }
 }
