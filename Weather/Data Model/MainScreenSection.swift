b1ff2a58e7cabceec7a49d9b63a88f339ebe4a17
Elina Lua Ming <ardentlycurious101@gmail.com>
2023-05-21 20:11:58 -0700

    Refactor `MainScreenSection` into separate file

---
+++ Weather/Data Model/MainScreenSection.swift
@@ -0,0 +1,13 @@
+//
+//  MainScreenSection.swift
+//  Weather
+//
+//  Created by Elina Lua Ming on 5/21/23.
+//
+
+import Foundation
+
+enum MainScreenSection: Int {
+    case userLocation = 0
+    case lastSearchedHistory = 1
+}
