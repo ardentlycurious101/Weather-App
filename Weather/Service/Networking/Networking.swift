d5fc9a5979b16c6b92fa578d408563be83a31e29
Elina Lua Ming <ardentlycurious101@gmail.com>
2023-05-21 21:15:36 -0700

    Remove` retryable`, add comments

--- Weather/Service/Networking/Networking.swift
+++ Weather/Service/Networking/Networking.swift
@@ -12,27 +12,23 @@ class Networking<T: TargetType> {
     let provider = MoyaProvider<T>()
     
     func fetch<R: Decodable>(api: T, completion: @escaping (_ results: R?, _ error: Error?) -> Void) {
-        do {
-            // Retryable up to 5 times if request fails with 3s retry interval.
-            let _ = try Retry.perform(maximumTries: 5, retryInterval: 3.0) {
-                self.provider.request(api) { result in
-                    switch result {
-                    case .success(let response):
-                        do {
-                            let decodedResult = try response.filterSuccessfulStatusCodes().map(R.self)
-                            completion(decodedResult, nil)
-                        } catch let error {
-                            print(error.localizedDescription)
-                            completion(nil, error)
-                        }
-                    case .failure(let error):
-                        print(error.localizedDescription)
-                        completion(nil, error)
-                    }
+        // Ideally we should also implement this to be retryable if request fails.
+        // Can use something like Combine's retry(_:)- https://developer.apple.com/documentation/combine/record/retry(_:)
+        
+        self.provider.request(api) { result in
+            switch result {
+            case .success(let response):
+                do {
+                    let decodedResult = try response.filterSuccessfulStatusCodes().map(R.self)
+                    completion(decodedResult, nil)
+                } catch let error {
+                    print(error.localizedDescription)
+                    completion(nil, error)
                 }
+            case .failure(let error):
+                print(error.localizedDescription)
+                completion(nil, error)
             }
-        } catch {
-            completion(nil, CustomError.general) // Can be more specific here by adding more cases to the CustomError enum
         }
     }
     
