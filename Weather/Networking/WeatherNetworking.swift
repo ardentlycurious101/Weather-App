2d7f308687f56957368c2cc8f092d2be9be45f70
Elina Lua Ming <ardentlycurious101@gmail.com>
2023-05-21 20:01:52 -0700

    Update WeatherNetworking: wrap request method with Retry

--- Weather/Networking/WeatherNetworking.swift
+++ Weather/Networking/WeatherNetworking.swift
@@ -12,21 +12,27 @@ class WeatherNetworking {
     let provider = MoyaProvider<WeatherAPI>()
     
     func fetchWeather(for location: (lat: String, lon: String), completion: @escaping (_ cityWeather: CityWeather?, _ error: Error?) -> Void) {
-        self.provider.request(.fetchWeather(location: location)) { result in
-            switch result {
-            case .success(let response):
-                do {
-                    let response = try response.filterSuccessfulStatusCodes()
-                    let cityWeather = try response.map(CityWeather.self)
-                    completion(cityWeather, nil)
-                } catch let error {
-                    print(error.localizedDescription)
-                    completion(nil, error)
+        do {
+            // Retryable up to 5 times if request fails with 3s retry interval.
+            let _ = try Retry.perform(maximumTries: 5, retryInterval: 3.0) {
+                self.provider.request(.fetchWeather(location: location)) { result in
+                    switch result {
+                    case .success(let response):
+                        do {
+                            let cityWeather = try response.filterSuccessfulStatusCodes().map(CityWeather.self)
+                            completion(cityWeather, nil)
+                        } catch let error {
+                            print(error.localizedDescription)
+                            completion(nil, error)
+                        }
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                        completion(nil, error)
+                    }
                 }
-            case .failure(let error):
-                print(error.localizedDescription)
-                completion(nil, error)
             }
+        } catch {
+            completion(nil, CustomError.general) // Can be more specific here by adding more cases to the CustomError enum
         }
     }
     
