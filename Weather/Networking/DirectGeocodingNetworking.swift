b57e7136ec82ccff58c8af52599079c5ff4ce7ab
Elina Lua Ming <ardentlycurious101@gmail.com>
2023-05-21 20:01:14 -0700

    add Retry wrapper to improve network request stability

--- Weather/Networking/DirectGeocodingNetworking.swift
+++ Weather/Networking/DirectGeocodingNetworking.swift
@@ -13,25 +13,29 @@ class DirectGeocodingNetworking {
     let provider = MoyaProvider<WeatherAPI>()
     
     func fetchResult(for input: String, completion: @escaping (_ result: [SearchResultElement]?, _ error: Error?) -> Void) {
-        print("fetch Result")
-        self.provider.request(.fetchCityList(input: input)) { result in
-            switch result {
-            case .success(let response):
-                do {
-                    print("success. Now decoding. \(response.statusCode)")
-
-                    let response = try response.filterSuccessfulStatusCodes()
-                    let results = try response.map([SearchResultElement].self)
-                    completion(results, nil)
-                } catch let error {
-                    print(error.localizedDescription)
-                    completion(nil, error)
+        do {
+            // Retryable up to 5 times if request fails with 3s retry interval.
+            let _ = try Retry.perform(maximumTries: 5, retryInterval: 3.0) {
+                self.provider.request(.fetchCityList(input: input)) { result in
+                    switch result {
+                    case .success(let response):
+                        do {
+                            let results = try response.filterSuccessfulStatusCodes().map([SearchResultElement].self)
+                            completion(results, nil)
+                        } catch let error {
+                            print(error.localizedDescription)
+                            completion(nil, error)
+                        }
+                    case .failure(let error):
+                        print(error.localizedDescription)
+                        completion(nil, error)
+                    }
                 }
-            case .failure(let error):
-                print(error.localizedDescription)
-                completion(nil, error)
             }
+        } catch {
+            completion(nil, CustomError.general) // Can be more specific here by adding more cases to the CustomError enum
         }
+        
     }
     
 }
