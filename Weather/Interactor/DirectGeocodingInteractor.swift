86afd1ee5638e513c148338ae5fad619c55af186
Elina Lua Ming <ardentlycurious101@gmail.com>
2023-05-21 20:00:23 -0700

    update debounce time to 200 ms for snappier experience

--- Weather/Interactor/DirectGeocodingInteractor.swift
+++ Weather/Interactor/DirectGeocodingInteractor.swift
@@ -10,10 +10,10 @@ import Foundation
 class DirectGeocodingInteractor {
     
     let networking = DirectGeocodingNetworking()
-    let debouncer = Debouncer(debounceTime: 0.3) // 300 ms debounce
+    let debouncer = Debouncer(debounceTime: 0.2) // 200 ms debounce
 
     func perform(for input: String, completion: @escaping (_ result: [SearchResultElement]?) -> Void) {
-        // Debounce input by 300 ms, so that we're not refetching with every new character typed to avoid making too many unnecessary API calls
+        // Debounce input by 200 ms, so that we're not refetching with every new character typed to avoid making too many unnecessary API calls
         debouncer.call {
             self.networking.fetchResult(for: input) { result, error in
                 guard error == nil else {
